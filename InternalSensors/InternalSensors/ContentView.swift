7ec187768f66d8bc2b76a43ac1ddf86162441e61
Mill3nium <milan-l97@hotmail.com>
2021-12-06 21:21:34 +0100

    Added new project

---
+++ InternalSensors/InternalSensors/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  InternalSensors
+//
+//  Created by Milan Languric on 2021-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+
---
+++ InternalSensors/InternalSensors/InternalSensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternalSensorsApp.swift
+//  InternalSensors
+//
+//  Created by Milan Languric on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct InternalSensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
